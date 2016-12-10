0601cd12ca5abd32a98b908214366366af8d434b
Itay Mishan <itay@platterz.ca>
2016-12-10 10:15:14 -0500

    adjust views + add filter

--- app/controllers/old_expenses_controller.rb
+++ app/controllers/old_expenses_controller.rb
@@ -1,12 +1,14 @@
 class OldExpensesController < ApplicationController
 
+  before_action :set_expenses, on: :index
+
   def create
     @old_expense = OldExpense.create(old_expense_params)
     render json: @old_expense, status: :created
   end
 
   def index
-    @old_expenses = OldExpense.order(:year, :month).last(10000).reverse
+    @old_expenses ||= OldExpense.last(50)
   end
 
   def new
@@ -15,6 +17,12 @@ class OldExpensesController < ApplicationController
 
   def old_expense_params
     params.require(:old_expense).permit(:month, :expense_type, :year, :category, :person, :amount, :comments, :currency)
+  end
+
+  def set_expenses
+    if params[:filter].present?
+      @old_expenses = OldExpense.where(year: params[:filter][:year], month: params[:filter][:month])
+    end
   end
 
 end
