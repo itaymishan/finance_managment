d6626d69d201f8217d5aa4e90341c907c9bf827d
Itay Mishan <itay@platterz.ca>
2016-12-25 13:14:22 -0500

    add migrations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  def self.currency_convert(from, to, amount)
+  def currency_convert(from, to, amount)
     return amount if from == to
     @rates ||= ExchangeRate.all
     @rates.find_by(from: from, to: to).rate * amount
