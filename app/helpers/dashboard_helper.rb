2e1974b02654d1e2b9f6ffe94e29446a2e87a96b
Itay Mishan <itay@platterz.ca>
2016-12-26 14:14:00 -0500

    change sort

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,6 +1,6 @@
 module DashboardHelper
   def expenses_by_category
-    bar_chart @expenses.group_by{|i| i.category.hebrew_name}.map{|k,v| [k, v.map{|item| item.currency.convert_to(params[:filter][:currency], item.amount) }.sum.to_i]}, height: "400px", library: {
+    bar_chart @expenses.group_by{|i| i.category.hebrew_name}.map{|k,v| [k, v.map{|item| item.currency.convert_to(params[:filter][:currency], item.amount) }.sum.to_i]}.sort_by{|item| -item[1]}, height: "400px", library: {
       title: {text: 'Sporters by age', x: -20},
       yAxis: {
          allowDecimals: false,
