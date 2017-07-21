f0a20713d1fe284712d9f7c249936a5b165201b9
itaymishan <itay@platterz.ca>
2017-07-21 17:54:39 -0400

    cleanup

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,16 +4,9 @@ class DashboardController < ApplicationController
   before_action :set_expenses, on: :index
   before_action :set_incomes, on: :index
 
-  def create
-
-  end
-
   def index
     @expenses ||= Expense.last(50).sort_by(&:created_at)
     @incomes ||= Income.last(50).sort_by(&:created_at)
-  end
-
-  def new
   end
 
   def set_expenses
